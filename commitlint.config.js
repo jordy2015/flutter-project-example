8c28f8aba79cbd3a981f5927e6639a8604ba06f0
Jordy Gonzalez <jordy.gonzalez@soyfri.com>
2023-09-16 16:08:43 -0600

    feat(login): Navbar changes [Ticket-CPT-60](https://www.google.com)

---
+++ commitlint.config.js
@@ -0,0 +1,22 @@
+
+const scope = [
+    'transfer',
+    'login',
+    'balance',
+    'kyc'
+]
+
+module.exports = {
+extends: ['@commitlint/config-conventional'],
+rules: {
+    'header-max-length': [2, 'always', Infinity],
+    'scope-enum': [2, 'always', scope],
+    'subject-case': [2, 'always', 'sentence-case'],
+    'references-empty': [2, 'never']
+},
+parserPreset: {
+    parserOpts: {
+        issuePrefixes: ['Ticket-']
+    }
+}
+}
